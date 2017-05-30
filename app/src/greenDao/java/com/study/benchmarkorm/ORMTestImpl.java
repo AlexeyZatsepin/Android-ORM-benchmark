ebade06d500591945c2492d8a9d8d4bf59086fe1
AlexeyZatsepin <alexzatsepin7@gmail.com>
2017-05-30 14:24:31 +0300

    GreenDao with InTx

--- app/src/greenDao/java/com/study/benchmarkorm/ORMTestImpl.java
+++ app/src/greenDao/java/com/study/benchmarkorm/ORMTestImpl.java
@@ -34,9 +34,10 @@ class ORMTestImpl extends ORMTest {
 
     @Override
     public void writeSimple(List<Book> books) {
-        for (Book book: books){
-            bookDao.insert(book);
-        }
+//        for (Book book: books){
+//            bookDao.insert(book);
+//        }
+        bookDao.insertInTx(books);
     }
 
     @Override
@@ -47,36 +48,38 @@ class ORMTestImpl extends ORMTest {
 
     @Override
     public void updateSimple(List<Book> books) {
-        for (Book book : books){
-            bookDao.update(book);
-        }
+//        for (Book book : books){
+//            bookDao.update(book);
+//        }
+        bookDao.updateInTx(books);
     }
 
     @Override
     public void deleteSimple(List<Book> books) {
-        for (Book book : books){
-            bookDao.delete(book);
-        }
+//        for (Book book : books){
+//            bookDao.delete(book);
+//        }
+        bookDao.deleteInTx(books);
     }
 
     @Override
     public void writeComplex(List<Library> libraries, List<Book> books, List<Person> persons) {
-        for (Book book: books){
-            bookDao.insert(book);
-        }
-        for (Person person: persons){
-            personDao.insert(person);
-        }
-        for (Library lib: libraries){
-            libraryDao.insert(lib);
-        }
+//        for (Book book: books){
+//            bookDao.insert(book);
+//        }
+        libraryDao.insertInTx(libraries);
+        bookDao.insertInTx(books);
+        personDao.insertInTx(persons);
+//        for (Person person: persons){
+//            personDao.insert(person);
+//        }
+//        for (Library lib: libraries){
+//            libraryDao.insert(lib);
+//        }
     }
 
     @Override
     public Pair<List<Library>, Pair<List<Book>, List<Person>>> readComplex(int librariesQuantity, int booksQuantity, int personsQuantity) {
-        bookDao.detachAll();
-        personDao.detachAll();
-        libraryDao.detachAll();
         List<Book> books = bookDao.queryBuilder().limit(booksQuantity).orderDesc(BookDao.Properties.Id).build().list();
         List<Person> persons = personDao.queryBuilder().limit(personsQuantity).orderDesc(PersonDao.Properties.Id).build().list();
         List<Library> libraries = libraryDao.queryBuilder().limit(librariesQuantity).orderDesc(LibraryDao.Properties.Id).build().list();
@@ -85,27 +88,33 @@ class ORMTestImpl extends ORMTest {
 
     @Override
     public void updateComplex(List<Library> libraries, List<Book> books, List<Person> persons) {
-        for (Book book: books){
-            bookDao.update(book);
-        }
-        for (Person person: persons){
-            personDao.update(person);
-        }
-        for (Library lib: libraries){
-            libraryDao.update(lib); //TODO try updateInTx
-        }
+//        for (Book book: books){
+//            bookDao.update(book);
+//        }
+//        for (Person person: persons){
+//            personDao.update(person);
+//        }
+//        for (Library lib: libraries){
+//            libraryDao.update(lib); //TODO try updateInTx
+//        }
+        bookDao.updateInTx(books);
+        personDao.updateInTx(persons);
+        libraryDao.updateInTx(libraries);
     }
 
     @Override
     public void deleteComplex(List<Library> libraries, List<Book> books, List<Person> persons) {
-        for (Book book: books){
-            bookDao.delete(book);
-        }
-        for (Person person: persons){
-            personDao.delete(person);
-        }
-        for (Library lib: libraries){
-            libraryDao.delete(lib);//TODO try deleteInTx
-        }
+        bookDao.deleteInTx(books);
+        personDao.deleteInTx(persons);
+        libraryDao.deleteInTx(libraries);
+// for (Book book: books){
+//            bookDao.delete(book);
+//        }
+//        for (Person person: persons){
+//            personDao.delete(person);
+//        }
+//        for (Library lib: libraries){
+//            libraryDao.delete(lib);//TODO try deleteInTx
+//        }
     }
 }
