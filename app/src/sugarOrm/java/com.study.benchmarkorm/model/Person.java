98cb30aadd5a3e59455e9825f2427000d2bbdcce
Vladislav <vladnamik@gmail.com>
2017-06-30 12:13:51 +0300

    Library getter&setter added

--- app/src/sugarOrm/java/com.study.benchmarkorm/model/Person.java
+++ app/src/sugarOrm/java/com.study.benchmarkorm/model/Person.java
@@ -79,6 +79,14 @@ public class Person extends SugarRecord{
     public void setPhone(long phone) {
         this.phone = phone;
     }
+    
+    public Library getLibrary() {
+        return library;
+    }
+
+    public void setLibrary(Library library) {
+        this.library = library;
+    }
 
     @Override
     public String toString() {
